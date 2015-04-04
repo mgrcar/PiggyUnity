217d81473b26f4a00fe31279b756aac509c0df90
mgrcar <mgrcar@gmail.com>
2015-04-05 00:26:32 +0200

    Instantiates and renders snowflakes

--- Assets/Common.cs
+++ Assets/Common.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Common 
@@ -12,5 +14,39 @@ public class Common
 
     public static readonly int BackgroundWidth 
         = 160;
+    public static readonly int NumFlakes
+        = 500;
     public static readonly int Scale;
+
+    public static Sprite CreateSpriteFrom(Texture2D srcTexture)
+    {
+        int w = srcTexture.width;
+        int h = srcTexture.height;
+        if (w % 2 != 0) { Debug.Log("ERROR: Pixel perfect sprite's width must be even!"); }
+        if (h % 2 != 0) { Debug.Log("ERROR: Pixel perfect sprite's height must be even!"); }
+        Color[] originalPixels = srcTexture.GetPixels();
+        List<Color> pixels = new List<Color>(w * Scale * h * Scale);
+        List<Color> row = new List<Color>(w * Scale);
+        for (int y = 0; y < h; y++)
+        {
+            for (int x = 0; x < w; x++)
+            {
+                row.AddRange(Enumerable.Repeat(originalPixels[w * y + x], Scale));
+            }
+            for (int r = 0; r < Scale; r++)
+            {
+                pixels.AddRange(row);
+            }
+            row.Clear();
+        }
+        Texture2D texture = new Texture2D(w * Scale, h * Scale, srcTexture.format, mipmap: false);
+        texture.SetPixels(pixels.ToArray());
+        texture.Apply();
+        return Sprite.Create(
+            texture,
+            new Rect(0, 0, texture.width, texture.height),
+            new Vector2(0.5f, 0.5f),
+            1
+        );
+    }
 }
---
+++ Assets/Snowfall.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class Snowfall : MonoBehaviour 
+{
+	void Start() 
+    {
+        Sprite snowflake1 = Common.CreateSpriteFrom((Texture2D)Resources.Load("snowflake1"));
+        Sprite snowflake2 = Common.CreateSpriteFrom((Texture2D)Resources.Load("snowflake2"));
+        
+        System.Random rnd = new System.Random();
+
+        for (var i = 0; i < Common.NumFlakes; i++) 
+        {
+            float x = (float)(rnd.NextDouble() * (double)Screen.width - (double)Screen.width / 2.0);
+            float y = (float)(rnd.NextDouble() * (double)Screen.height - (double)Screen.height / 2.0);
+            GameObject snowflake = (GameObject)UnityEngine.Object.Instantiate(GameObject.Find("Snowflake"), new Vector3(x, y, -1), new Quaternion());
+            SpriteRenderer renderer = snowflake.GetComponent<SpriteRenderer>();
+            renderer.sprite = Math.Round(rnd.NextDouble()) == 0 ? snowflake1 : snowflake2;
+            //sprite.$angle = Math.random() * angleRange + 1.5708 - angleRange / 2;
+            //sprite.$inc = Math.random() * scale(0.2) + scale(0.2); 
+            //sprite.$x = x;
+            //sprite.$y = y;
+        }
+	}
+}
