b943fa22c7567ee7478e5a311d24f6df18a00224
mgrcar <mgrcar@gmail.com>
2015-04-05 13:13:14 +0200

    Snowfall working

--- Assets/Snowfall.cs
+++ Assets/Snowfall.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class Snowfall : MonoBehaviour 
 {
-    private System.Random Rnd 
+    private System.Random Rnd
         = new System.Random();
 
 	void Start() 
@@ -23,28 +23,13 @@ public class Snowfall : MonoBehaviour
             SpriteRenderer renderer = snowflake.GetComponent<SpriteRenderer>();
             renderer.sprite = Math.Round(Rnd.NextDouble()) == 0 ? snowflake1 : snowflake2;
             SnowflakeProperties prop = snowflake.GetComponent<SnowflakeProperties>();
-            prop.Angle = Rnd.NextDouble() * Common.AngleRange + 1.5708 - Common.AngleRange / 2.0;
+            prop.Angle = Rnd.NextDouble() * Common.AngleRange + 1.57079632679 - Common.AngleRange / 2.0;
             prop.Speed = Rnd.NextDouble() * 0.2 + 0.2;
         }
 	}
 
     void FixedUpdate()
     {
-        //for (i = 0; i < flakes.children.length; i++) {
-        //    flake = flakes.children[i];
-        //    flake.$x = flake.$x + flake.$inc * Math.cos(flake.$angle);
-        //    flake.$y = flake.$y + flake.$inc * Math.sin(flake.$angle);
-        //    flake.x = Math.round(flake.$x);
-        //    flake.y = Math.round(flake.$y);
-        //    flake.$angle += (Math.random() * 2 - 1) * angleChange;
-        //    flake.$angle = Math.max(angleMin, Math.min(angleMax, flake.$angle));
-        //    if (flake.y > ch + scale(5)) { 
-        //        flake.y = flake.$y = -scale(5);
-        //        flake.$x = Math.random() * cw;
-        //        flake.x = Math.round(flake.$x);
-        //        flake.$angle = Math.random() * angleRange + 1.5708 - angleRange / 2;
-        //    }
-        //}
         foreach (GameObject snowflake in GameObject.FindGameObjectsWithTag("Snowflake"))
         {
             SnowflakeProperties prop = snowflake.GetComponent<SnowflakeProperties>();
@@ -53,7 +38,13 @@ public class Snowfall : MonoBehaviour
             pos.Y = (float)((double)pos.Y - prop.Speed * Math.Sin(prop.Angle));
             prop.Angle += (Rnd.NextDouble() * 2.0 - 1.0) * Common.AngleChange;
             prop.Angle = Math.Max(Common.AngleMin, Math.Min(Common.AngleMax, prop.Angle));
-            // ...
+            double hbh = (double)Common.BackgroundHeight / 2.0 + 0.5;
+            double bw = Common.BackgroundWidth;
+            if (pos.Y < -hbh) 
+            { 
+                pos.Y = (float)hbh;
+                pos.X = (float)(Rnd.NextDouble() * bw - bw / 2.0);
+            }
         }
     }
 }
